b801274a59901385f1fb0c79bb0cd80945300f05
OmarElmorsy <119528435+OmarElmorsy@users.noreply.github.com>
2023-05-30 15:52:56 +0300

    newVersion

--- main.js
+++ main.js
@@ -1,38 +1,36 @@
-let arrayId = [];
+let cardID = [];
 let index =0;
 const pageBody = document.getElementById("pagebody");
-let contener = document.createElement("div");
-contener.id = "contener";
+
+
 const plus =document.querySelector("#plus"); 
 
 // add Some Information in Page 
+// pageBody.innerHTML = `
+//   <h1> Some Informantion : </h1>
+//   <h2>Click In Button "+" To Add Card to Chage Color of Page <\h2>
+//   <h2>Set Color Name In Input Field And Click In Button Change<\h2>
+// `; 
 
-pageBody.innerHTML = `<h1> Some Informantion : </h1>
-<h2>Click In Button "+" To Add Card to Chage Color of Page <\h2>
-<h2>Set Color Name In Input Field And Click In Button Change<\h2>`; 
-
-pageBody.append(contener);
 pageBody.append(plus);
 
 // Handler Button Plus 
-plus.addEventListener("click",createElement);
-function createElement(){
+plus.addEventListener("click",() => {
+  let newCardInformation = {divId :`div${index}`,  inputId :`input${index}`, buttonx :`buttonx${index}`, buttont :`buttont${index}`};
+  cardID.push(JSON.stringify(newCardInformation));
+  createCard(index);
   index+=1;
-  let temp = {divId :`div${index}`,  inputId :`input${index}`, buttonx :`buttonx${index}`, buttont :`buttont${index}`};
-  arrayId.push(JSON.stringify(temp));
-    createCard(index-1);
-}
+});
 
 // Create Card  
 function createCard (index){
-  let obj = JSON.parse(arrayId[index]);
+  let obj = JSON.parse(cardID[index]);
   let card = document.createElement("div");
   card.id = obj.divId;
-  card.innerHTML =  `<input id =${obj.inputId}> <button id =${obj.buttont} onclick = "chagecolor(${obj.inputId})">change</button><button id =${obj.buttonx} onclick = "deleteCard(${obj.divId})" >x</button>`;
-  contener.append(card);
+  card.setAttribute("onclick", `chagecolor(${obj.inputId})` )
+  card.innerHTML =`<input id =${obj.inputId}> <p style="display: inline;"  id =${obj.buttonx} onclick = "deleteCard(${obj.divId})" >x</p>`;
+  pageBody.insertBefore(card, plus);
 }
-
-
 function chagecolor(element){
   let inputfind = document.getElementById(`${element.id}`);
   if(inputfind.value === "") {
@@ -49,15 +47,11 @@ function chagecolor(element){
 
 } 
 function deleteCard (element){
-  
   let card = document.getElementById(`${element.id}`);
     card.remove();
-  
 }
 function isValidColor(strColor) {
   var s = new Option().style;
   s.color = strColor;
-
-  // return 'false' if color wasn't assigned
   return s.color == strColor.toLowerCase();
 }
